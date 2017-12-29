1ac9ebf59d8b72603dffb99cf485396ba5a23db4
Dominique Miller <dominique.p.miller@gmail.com>
2017-12-28 22:01:51 -0500

    postSchema update

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -2,9 +2,9 @@ import * as mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  // author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   body: { type: String, required: true },
-  comments: [ {type: mongoose.Schema.Types.ObjectId, ref: "Comments" } ]
+  comments: [ {type: mongoose.Schema.Types.ObjectId, ref: "Comment" } ]
 }, { timestamps: true });
 
 const Post = mongoose.model("Post", postSchema);
