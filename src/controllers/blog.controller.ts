1695c0de3b5bcb25b00623e7bf7389d17ebb3479
Dominique Miller <dominique.p.miller@gmail.com>
2017-12-28 22:02:14 -0500

    index, show and updatre controller actions

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -3,26 +3,44 @@ import { default as Post } from "../models/post.model";
 import { default as User } from "../models/user.model";
 import * as boom from "boom";
 
-export let index = (req: Request, res: Response, next: NextFunction) => {
-  Post.find({}, (err, posts) => {
-    if (err) next(boom.notFound(err));
+User.find();
 
-    res.json(posts);
-  });
+export let index = (req: Request, res: Response, next: NextFunction) => {
+  Post.find()
+      .populate({ path: "author" })
+      .exec( (err, posts) => {
+        if (err) next(boom.notFound(err));
+
+        res.json(posts);
+      });
 };
 
 export let create = (req: Request, res: Response, next: NextFunction) => {
+
   Post.create( req.body, (err, post) => {
-    if (err) next(boom.notFound(err));
+    if (err) next(boom.badRequest(err));
 
     res.json(post);
   });
 };
 
 export let update = (req: Request, res: Response, next: NextFunction) => {
-  res.send("update blog");
+  Post.findById(req.params.id, (err, post) => {
+    if (err) next(boom.notFound(err));
+    post = Object.assign({}, post, req.body);
+    post.save( (err) => {
+      if (err) next(boom.badData(err));
+      res.json("Post updated successfully");
+    })
+  });
 };
 
 export let show = (req: Request, res: Response, next: NextFunction) => {
-  res.send("show individual blog");
+  Post.findById(req.params.id)
+      .populate("author")
+      .populate("comments")
+      .exec( (err, post) => {
+        if (err) next(boom.notFound(err));
+        res.json(post);
+      });
 };
