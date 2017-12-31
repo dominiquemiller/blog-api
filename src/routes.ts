98d1dc13141392d8d6cc9bd31baea60178d9642b
Dominique Miller <dominique.p.miller@gmail.com>
2017-12-31 15:15:18 -0500

    POST route for comment creation.

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,19 @@
 import { Express } from "express";
 import * as blogController from "./controllers/blog.controller";
 import { errorHandler } from "./middleware/error.handler";
+import * as commentController from "./controllers/comment.controller";
+
 export function routes(app: Express) {
 
   app.route("/api/blogs")
     .get(blogController.index)
-    .post(blogController.create)
-    .patch(blogController.update);
+    .post(blogController.create);
 
-  app.get("/api/blog/:id", blogController.show);
+  app.route("/api/blog/:id")
+     .get(blogController.show)
+     .patch(blogController.update);
+
+  app.post("/api/comment", commentController.create );
 
   app.use(errorHandler);
 
