e741f08e3f9f72f0e23e2f8f7478428334bbed76
Dominique Miller <dominique.p.miller@gmail.com>
2018-01-08 21:59:29 -0500

    User model interface.  User model encrypt password before saving.  User model method to compare passwords.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,39 @@
-import * as bcyrpt from "bcrypt-nodejs";
+import * as bcrypt from "bcrypt-nodejs";
 import * as mongoose from "mongoose";
+
+export type UserModel = mongoose.Document & {
+  email: string,
+  password: string,
+  comparePassword: (candidatePassword: string, cb: (err: any, isMatch: any) => {}) => void
+};
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true }
 }, { timestamps: true });
 
-// add encrypting and compare password functions below
+/**
+ * Password hash middleware.
+ */
+userSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword: string, cb: (err: any, isMatch: any) => {}) {
+  bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
+    cb(err, isMatch);
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
