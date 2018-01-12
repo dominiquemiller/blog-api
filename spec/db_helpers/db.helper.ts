9a9866f8b8d5efc1f2e0ad09763b7e2df6136e13
Dominique Miller <dominique.p.miller@gmail.com>
2018-01-12 13:40:23 -0500

    Jasmine comfig.  Mongo test helpers.  User model helper.

---
+++ spec/db_helpers/db.helper.ts
@@ -0,0 +1,36 @@
+import * as mongoose from "mongoose";
+// fixtures
+import { users } from "../fixtures/users.fixture";
+// models
+import { default as User } from "../../src/models/user.model";
+
+export interface ModelSeed {
+  name: mongoose.Model<any>;
+  data: Array<any>;
+}
+
+export interface Models {
+  [key: string]: ModelSeed;
+}
+
+const models: Models = { "User": { name: User, data: users } };
+
+export const dropDB = (): void => {
+  for (const key in models) {
+    models[key].name.collection.drop();
+  }
+};
+
+export const seedModel = (name: string) => {
+  return new Promise( (res, rej) => {
+    models[name].data.forEach( (item, index) => {
+      const count = index + 1;
+      const length = models[name].data.length;
+      models[name].name.create(item, (err, doc) => {
+        if (count === length) {
+          res(doc);
+        }
+      });
+    });
+  });
+};
---
+++ spec/fixtures/users.fixture.ts
@@ -0,0 +1,6 @@
+export const users = [
+  {
+    "email": "dom@trimagency.com",
+    "password": "password"
+  }
+];
