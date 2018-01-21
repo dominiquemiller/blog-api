35f9db387a0449357d842d2576ffe9334dae42ef
Dominique Miller <dominique.p.miller@gmail.com>
2018-01-21 12:08:39 -0500

    Db helper spec method to create test posts with attached author.

--- spec/db_helpers/db.helper.ts
+++ spec/db_helpers/db.helper.ts
@@ -1,8 +1,10 @@
 import * as mongoose from "mongoose";
 // fixtures
 import { users } from "../fixtures/users.fixture";
+import { posts } from "../fixtures/posts.fixture";
 // models
-import { default as User } from "../../src/models/user.model";
+import { default as User, UserModel } from "../../src/models/user.model";
+import { default as Post } from "../../src/models/post.model";
 
 export interface ModelSeed {
   name: mongoose.Model<any>;
@@ -13,7 +15,8 @@ export interface Models {
   [key: string]: ModelSeed;
 }
 
-const models: Models = { "User": { name: User, data: users } };
+const models: Models = { "User": { name: User, data: users },
+                         "Posts": { name: Post, data: [] } };
 
 export const dropDB = (): void => {
   for (const key in models) {
@@ -23,11 +26,26 @@ export const dropDB = (): void => {
 };
 
 export const seedModel = (name: string) => {
-  return new Promise( (res, rej) => {
+  return new Promise<UserModel>( (res, rej) => {
     models[name].data.forEach( (item, index) => {
       const count = index + 1;
       const length = models[name].data.length;
-      models[name].name.create(item, (err, doc) => {
+      models[name].name.create(item, (err, doc: UserModel) => {
+        if (count === length) {
+          res(doc);
+        }
+      });
+    });
+  });
+};
+
+export const seedPosts = (userId: string) => {
+  return new Promise( (res, rej) => {
+    posts.forEach( (item, index) => {
+      const count = index + 1;
+      const length = posts.length;
+      const post = Object.assign({}, item, { author: userId });
+      Post.create(post, (err: any, doc: any) => {
         if (count === length) {
           res(doc);
         }
---
+++ spec/fixtures/posts.fixture.ts
@@ -0,0 +1,17 @@
+export const posts = [
+  {
+    title: "my test post",
+    body: "test post body",
+    author: ""
+   },
+   {
+    title: "my second post",
+    body: "test post body",
+    author: ""
+   },
+   {
+    title: "my third post",
+    body: "test post body",
+    author: ""
+   }
+];
