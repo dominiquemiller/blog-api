e053ce734eb9ae58afe97a7942df5caba73de6dc
Dominique Miller <dominique.p.miller@gmail.com>
2018-01-23 21:56:33 -0500

    Interface for Post Model.

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,5 +1,12 @@
 import * as mongoose from "mongoose";
 
+export type PostModel = mongoose.Document & {
+  title: string;
+  author: string;
+  body: string;
+  comments: string[];
+};
+
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
