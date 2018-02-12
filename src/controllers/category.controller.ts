3e4e8e0c4efebc9821c08c09549456662b131ab4
Dominique Miller <dominique.p.miller@gmail.com>
2018-02-11 22:05:21 -0500

    Category and Tag controller scaffold.

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,12 @@
+import { Response, Request, NextFunction } from "express";
+import { jwtOptions } from "../config/passport";
+import * as jwt from "jsonwebtoken";
+import * as boom from "boom";
+
+export let create = () => {};
+
+export let update = () => {};
+
+export let index = () => {};
+
+export let show = () => {};
