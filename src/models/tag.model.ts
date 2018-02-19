8ce9f370ebd706c5b5c78694aead90a885b38a41
Dominique Miller <dominique.p.miller@gmail.com>
2018-02-19 11:49:29 -0500

    Update TagModel type with posts array.

--- src/models/tag.model.ts
+++ src/models/tag.model.ts
@@ -2,6 +2,7 @@ import * as mongoose from "mongoose";
 
 export type TagModel = mongoose.Document & {
   title: string;
+  posts?: Array<mongoose.Schema.Types.ObjectId>;
 };
 
 const tagSchema = new mongoose.Schema({
