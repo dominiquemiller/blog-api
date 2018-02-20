67fa0f95fec04fbeb78ddc317fc259326663cc46
Dominique Miller <dominique.p.miller@gmail.com>
2018-02-19 21:29:20 -0500

    Add virtuals true to Cat schema.  GET single category spec.

--- spec/integration/category-route.spec.ts
+++ spec/integration/category-route.spec.ts
@@ -6,6 +6,8 @@ import * as dbHelpers from "../helpers/db.helper";
 const tellJasmineDone = require("jasmine-supertest");
 import {} from "jasmine";
 
+import { Category, CategoryModel } from "../../src/models/category.model";
+
 describe("Category route", () => {
 
   beforeAll( async (done) => {
@@ -21,10 +23,29 @@ describe("Category route", () => {
 
   it("GET should return a list of categories", (done) => {
 
+    function countCheck(docArray: Array<any>) {
+       return docArray.every( (value: any, index: number) => {
+        return value.count === 0;
+      });
+    }
+
     supertest(server).get("/api/categories")
                      .expect(200)
                      .expect( (res: any) => {
-                       if (res.body.length !== 4) throw new Error("Incoreect number of posts returned"); })
+                       if (res.body.length !== 4) throw new Error("Incoreect number of posts returned");
+                       if (!countCheck(res.body)) throw new Error("Incorrect count of posts for each category");
+                     })
+                     .end(tellJasmineDone(done));
+  });
+
+  it("GET should return a category", async(done) => {
+    const cat = await Category.findOne({ title: "General" });
+
+    supertest(server).get(`/api/categories/${cat._id}`)
+                     .expect(200)
+                     .expect( (res: any) => {
+                       if (res.body.title !== "General") throw new Error("Incorrect category returned");
+                     })
                      .end(tellJasmineDone(done));
   });
 
--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -8,7 +8,7 @@ export type CategoryModel = mongoose.Document & {
 const categorySchema = new mongoose.Schema({
   title: { type: String, required: true },
   posts: [ { type: mongoose.Schema.Types.ObjectId, ref: "Post" } ]
-}, { timestamps: true });
+}, { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } });
 
 categorySchema.virtual("count").get(function() { return this.posts.length; });
 
