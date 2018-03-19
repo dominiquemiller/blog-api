73c2a800f5609aa951d75981bc861b7e92d4f982
Dominique Miller <dominique.p.miller@gmail.com>
2018-03-18 22:39:25 -0400

    delayed job service

---
+++ src/services/delayed-jobs.service.ts
@@ -0,0 +1,36 @@
+import * as kue from "kue";
+import { config } from "../config";
+import { MediaSizes } from "../services/media-sizes.service";
+const env = process.env.NODE_ENV;
+const envConfig = config[env];
+
+const redis = { redis: envConfig.redis.connect };
+const queue = kue.createQueue(redis);
+
+function resizeImage(data: any) {
+  const job = queue.create("resizeImage", data)
+    .priority("critical")
+    .attempts(8)
+    .backoff(true)
+    .save();
+
+  job.on("complete", () => {
+    console.log("completed");
+  }).on("failed", () => {
+    console.error("job failed");
+  }) ;
+}
+
+queue.process("resizeImage", 1, async (job, done) => {
+    const mediaService = MediaSizes.getInstance();
+    const key = await mediaService.process(job.data);
+    // do work here
+    done();
+});
+
+// add more types of jobs to this object
+export const job = {
+  media: (data: any) => {
+    resizeImage(data);
+  }
+};
